11cf4c61a0eb6297b3fec2aee9e56b90468913b0
Vincent OLeary <v@vincentoleary.com>
2019-09-06 16:57:58 -0400

    Big updates, see other branch for details

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20190426161943";
+var CACHE_NAME = "pixyll2-20190906124341";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201904261619",
+        "/css/pixyll.css?201909061243",
         "/"
       ]);
     })
