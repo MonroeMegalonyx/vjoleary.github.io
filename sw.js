33203c6deaaf9d5241ab1da710a81714f4e29884
Vincent OLeary <7621080+vincentoleary@users.noreply.github.com>
2023-12-28 15:26:05 -0500

    Moved hosts back to GitHub
    Copied over any changes from Fastmail

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20190908180952";
+var CACHE_NAME = "pixyll2-20210208185629";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201909081809",
+        "/css/pixyll.css?202102081856",
         "/"
       ]);
     })
